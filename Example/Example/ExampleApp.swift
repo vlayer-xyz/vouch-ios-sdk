061ec48295865ace7b03f6d294d3727c96dc1bbc
k.moczala <k.moczala@nomtek.com>
2026-01-19 10:05:33 +0100

    Add empty example application

---
+++ Example/Example/ExampleApp.swift
@@ -0,0 +1,17 @@
+//
+//  ExampleApp.swift
+//  Example
+//
+//  Created by Krzysztof Moczała on 19/01/2026.
+//
+
+import SwiftUI
+
+@main
+struct ExampleApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
