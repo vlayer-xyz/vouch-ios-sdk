22b6e90f14740b3670c64984c13478401e74a26a
k.moczala <k.moczala@nomtek.com>
2026-04-23 15:43:30 +0200

    Update VouchSDK to version v0.4.3

--- Sources/VouchSDK.xcframework/ios-arm64/VouchSDK.framework/Common_Common.bundle/Runner.js
+++ Sources/VouchSDK.xcframework/ios-arm64/VouchSDK.framework/Common_Common.bundle/Runner.js
@@ -1,19 +1,26 @@
-if (typeof main === "function") {
-  window.sendEvent({
-    type: "Telemetry",
-    payload: {
-      event: "PROCESSING_NEW_PAGE",
-      metadata: {
-        url: window.location.href,
-      },
-    },
-  });
-
-  if (document.readyState === "complete") {
-    main();
-  } else {
-    window.onload = function () {
+let vouch_runMain = function () {
+  if (
+    document.readyState === "complete" ||
+    document.readyState === "interactive"
+  ) {
+    if (typeof main === "function") {
       main();
-    };
+    }
+  } else {
+    setTimeout(() => {
+      vouch_runMain();
+    }, 1000);
   }
-}
+};
+
+window.sendEvent({
+  type: "Telemetry",
+  payload: {
+    event: "PROCESSING_NEW_PAGE",
+    metadata: {
+      url: window.location.href,
+    },
+  },
+});
+
+vouch_runMain();
